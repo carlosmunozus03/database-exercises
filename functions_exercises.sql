e81e813b9fe20e5e7ab51aa42264a90f45df110b
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-06-24 17:11:55 -0500

    fiex error

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,60 +1,5 @@
-#
-Part One
-
-USE employees;
-
-SELECT *
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
-
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%';
-
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%';
-
-#
-Part Two
-
-SELECT *
-FROM employees
-WHERE first_name = 'Irena'
-   OR first_name = 'Vidya'
-   OR first_name = 'Maya';
-
-SELECT *
-FROM employees
-WHERE (first_name = 'Irena'
-    OR first_name = 'Vidya'
-    OR first_name = 'Maya')
-  AND gender = 'M';
-
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%'
-   OR last_name LIKE '%e';
-
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%'
-  AND last_name LIKE '%e';
-
-#
-or you can do this:
-
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%e';
-
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%'
-  AND last_name NOT LIKE '%qu%';
-
-#
-Order By exercises
+USE
+employees;
 
 SELECT *
 FROM employees
@@ -81,34 +26,7 @@ FROM employees
 WHERE last_name LIKE 'E%'
 ORDER BY emp_no DESC;
 
-#
-Functions exercises
-
-SELECT CONCAT(first_name, ' ', last_name)
+SELECT *
 FROM employees
 WHERE last_name LIKE 'E%'
-  AND last_name LIKE '%e';
-
-SELECT *
-FROM employees
-WHERE DAY (birth_date) = 25 AND MONTH (birth_date) = 12;
-
-SELECT *
-FROM employees
-WHERE DAY (birth_date) = 25
-  AND MONTH (birth_date) = 12
-  AND YEAR (hire_date) LIKE '199%';
-
-SELECT *
-FROM employees
-WHERE DAY (birth_date) = 25
-  AND MONTH (birth_date) = 12
-  AND YEAR (hire_date) LIKE '199%'
-ORDER BY birth_date, hire_date DESC;
-
-SELECT *, DATEDIFF(hire_date, CURDATE())
-FROM employees
-WHERE DAY (birth_date) = 25
-  AND MONTH (birth_date) = 12
-  AND YEAR (hire_date) LIKE '199%'
-ORDER BY DATEDIFF(hire_date, CURDATE());
+ORDER BY emp_no ASC;
