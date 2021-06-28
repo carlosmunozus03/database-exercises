ff8d95a586d8bd957668e462c957e2028679eb2a
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-06-28 09:47:31 -0500

    subqueries exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -13,19 +13,3 @@ CREATE TABLE albums
     genre        VARCHAR(50),
     PRIMARY KEY (id)
 );
-
-# USE
-#     codeup_test_db;
-#
-# DROP TABLE IF EXISTS albums;
-#
-# CREATE TABLE albums
-# (
-#     id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
-#     artist       VARCHAR(50),
-#     name         VARCHAR(100),
-#     release_date INT,
-#     sales        DECIMAL(15, 2),
-#     genre        VARCHAR(50),
-#     PRIMARY KEY (id)
-# );
---
+++ subqueries_exercises.sql
@@ -0,0 +1,62 @@
+USE employees;
+
+-- EXAMAMPLE --
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+)
+LIMIT 10;
+
+# 1.-
+SELECT *
+FROM employees
+WHERE hire_date = (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = 101010
+);
+#2.-
+SELECT title
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+);
+
+#3.-
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date > NOW()
+)
+  AND gender = 'F';
+
+-- BONUS --
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+    SELECT dept_no
+    FROM dept_manager
+    WHERE emp_no IN (
+        SELECT emp_no
+        FROM employees
+        WHERE gender = 'F'
+    )
+      AND to_date > NOW()
+);
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no = (
+    SELECT emp_no
+    FROM salaries
+    WHERE salary = (
+        SELECT MAX(salary)
+        FROM salaries
+    )
+);
