9ba5198b59c45ea784cf9849630556fe1de5e67b
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-07-07 10:33:28 -0500

    complete and fix files

--- db_design_lec.sql
+++ db_design_lec.sql
@@ -1,3 +1,25 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS quotes;
+
+CREATE TABLE quotes
+(
+    id                INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    author_first_name VARCHAR(50),
+    author_last_name  VARCHAR(100) NOT NULL,
+    content           TEXT         NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO quotes (author_first_name, author_last_name, content)
+VALUES ('Douglas', 'Adams', 'I love deadlines. I love the whooshing noise they make as they go by.'),
+       ('Douglas', 'Adams', 'Don''t Panic.'),
+       ('Douglas', 'Adams', 'Time is an illusion. Lunchtime doubly so.'),
+       ('Mark', 'Twain', 'Clothes make the man. Naked people have little or no influence on society.'),
+       ('Kurt', 'Vonnegut', 'The universe is a big place, perhaps the biggest.');
+
+DROP TABLE IF EXISTS authors;
+
 CREATE TABLE authors
 (
     id         INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -11,6 +33,11 @@ VALUES ('Douglas', 'Adams'),
        ('Mark', 'Twain'),
        ('Kurt', 'Vonnegut');
 
+SELECT *
+FROM authors;
+
+DROP TABLE IF EXISTS quotes;
+
 CREATE TABLE quotes
 (
     id        INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -20,7 +47,8 @@ CREATE TABLE quotes
     FOREIGN KEY (author_id) REFERENCES authors (id)
 );
 
-# Seeds
+# seeds
+
 INSERT INTO quotes (author_id, content)
 VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'),
         'I love deadlines. I love the whooshing noise they make as they go by.');
@@ -36,10 +64,21 @@ values ((select id from authors where first_name = 'Kurt' and last_name = 'Vonne
 INSERT INTO quotes (author_id, content)
 VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'), 'Don''t Panic.');
 
+SELECT *
+FROM quotes;
+
+SELECT quotes.content AS 'content',
+       CONCAT(authors.first_name, ' ', authors.last_name)
+                      AS 'author name'
+FROM quotes
+         JOIN authors ON authors.id = author_id;
+
+DROP TABLE IF EXISTS topics;
+
 CREATE TABLE topics
 (
     id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255),
+    name VARCHAR(50),
     PRIMARY KEY (id)
 );
 
@@ -49,10 +88,13 @@ VALUES ('Space and Time'),
        ('Office Life'),
        ('Hitchiker''s Guide to the Galaxy');
 
+SELECT *
+FROM topics;
+
 CREATE TABLE quote_topic
 (
-    quote_id INTEGER UNSIGNED NOT NULL,
-    topic_id INTEGER UNSIGNED NOT NULL,
+    quote_id INT UNSIGNED NOT NULL,
+    topic_id INT UNSIGNED NOT NULL,
     FOREIGN KEY (quote_id) REFERENCES quotes (id),
     FOREIGN KEY (topic_id) REFERENCES topics (id)
 );
@@ -67,3 +109,9 @@ VALUES (1, 2),
        (3, 4),
        (4, 2),
        (5, 1);
+
+SELECT first_name, last_name, content, t.name
+FROM quotes
+         JOIN quote_topic qt ON quotes.id = qt.quote_id
+         JOIN topics t ON qt.topic_id = t.id
+         JOIN authors a ON quotes.author_id = a.id;
--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,63 +1,39 @@
-USE employees;
+use employees;
 
--- EXAMAMPLE --
-SELECT first_name, last_name, birth_date
-FROM employees
-WHERE emp_no IN (
-    SELECT emp_no
-    FROM dept_manager
-)
-LIMIT 10;
--- EXAMAMPLE --
-
-
-# 1.-
 SELECT *
 FROM employees
-WHERE hire_date = (
-    SELECT hire_date
-    FROM employees
-    WHERE emp_no = 101010
-);
-#2.-
+WHERE hire_date IN (SELECT hire_date FROM employees WHERE emp_no = '101010');
+
 SELECT title
 FROM titles
-WHERE emp_no IN (
-    SELECT emp_no
-    FROM employees
-    WHERE first_name = 'Aamod'
-);
+WHERE emp_no IN (SELECT emp_no FROM employees WHERE first_name = 'Aamod');
 
-#3.-
+SELECT DISTINCT title
+FROM titles
+WHERE emp_no IN (SELECT emp_no FROM employees WHERE first_name = 'Aamod');
+
 SELECT first_name, last_name
 FROM employees
-WHERE emp_no IN (
-    SELECT emp_no
-    FROM dept_manager
-    WHERE to_date > NOW()
-)
+WHERE emp_no IN (SELECT emp_no FROM dept_manager WHERE to_date LIKE '9999%')
   AND gender = 'F';
 
--- BONUS --
 SELECT dept_name
 FROM departments
 WHERE dept_no IN (
     SELECT dept_no
     FROM dept_manager
-    WHERE emp_no IN (
+    WHERE to_date > NOW()
+      AND emp_no IN (
         SELECT emp_no
         FROM employees
-        WHERE gender = 'F'
-    )
-      AND to_date > NOW()
-);
+        WHERE gender = 'F'));
 
 SELECT first_name, last_name
 FROM employees
-WHERE emp_no = (
+WHERE emp_no IN (
     SELECT emp_no
     FROM salaries
-    WHERE salary = (
+    WHERE salary IN (
         SELECT MAX(salary)
         FROM salaries
     )
